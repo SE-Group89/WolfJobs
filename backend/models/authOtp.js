8078ac40a0deff0ebcb656454f539c3cd3b68f32
Naisarg Shah <naisargshah12@gmail.com>
2024-11-25 22:36:10 -0500

    details

--- backend/models/authOtp.js
+++ backend/models/authOtp.js
@@ -1,25 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // Importing Mongoose library for database schema and model creation
 
-// Auth OTP table
+// Define schema for storing OTP (One-Time Password) data
 const autoOtpSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true
+  userId: {    // The ID of the user associated with this OTP
+    type: String, // The user ID is stored as a string (can be changed to ObjectId for relational reference)
+    required: true // This field is mandatory
   },
-  otp: {
-    type: String,
-    required: true
+  otp: { // The OTP value
+    type: String, // OTP is stored as a string
+    required: true // This field is mandatory
   },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  createdAt: { // The date and time when this OTP record was created
+    type: Date, // Stores date and time
+    default: Date.now // Defaults to the current date and time if not provided
   }
 
 }, {
-  timestamps: true
+  timestamps: true // Automatically adds `createdAt` and `updatedAt` fields to the schema
 });
 
-
+// Create the Mongoose model for the schema
+// 'autoOtp' is the name of the collection in the database
 const autoOtp = mongoose.model('autoOtp', autoOtpSchema);
 
+// Export the model so it can be used in other parts of the application
 module.exports = autoOtp;
