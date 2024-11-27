33f53fc645c3807001c23a2a8995d606888c6e7b
Naisarg Shah <naisargshah12@gmail.com>
2024-11-26 19:58:19 -0500

    fixing minor problems

--- frontend/src/store/ApplicationStore.ts
+++ frontend/src/store/ApplicationStore.ts
@@ -1,19 +1,24 @@
+// Import zustand for state management
 import { create } from "zustand";
 
+// Define the shape of the application state
 type ApplicationState = {
-  applicationList: Application[];
+  applicationList: Application[];  // List of applications, with each application of type 'Application'
 };
 
+// Define actions for updating the application state
 type ApplicationAction = {
-  updateApplicationList: (name: ApplicationState["applicationList"]) => void;
+  updateApplicationList: (applicationList: ApplicationState["applicationList"]) => void;  // Action to update the application list
 };
 
+// Create the zustand store combining state and actions for managing applications
 export const useApplicationStore = create<
-  ApplicationState & ApplicationAction
+  ApplicationState & ApplicationAction  // Define the store combining state and actions
 >()((set) => ({
-  applicationList: [],
+  applicationList: [],  // Initial state: empty application list
 
+  // Action to update the application list state
   updateApplicationList: (applicationList: Application[]) => {
-    set(() => ({ applicationList: applicationList }));
+    set(() => ({ applicationList: applicationList }));  // Update the application list with the new value
   },
 }));
--- frontend/src/store/JobStore.ts
+++ frontend/src/store/JobStore.ts
@@ -1,17 +1,26 @@
+// Importing 'create' from 'zustand', a state management library for React
 import { create } from "zustand";
 
+// Type definition for the state of the job store
 type JobState = {
+  // jobList: an array of Job objects, representing the list of jobs in the store
   jobList: Job[];
 };
 
+// Type definition for the actions that can be performed on the store
 type JobAction = {
+  // updateJobList: A function to update the jobList state with a new list of jobs
   updateJobList: (name: JobState["jobList"]) => void;
 };
 
+// Creating the store using zustand's 'create' function
 export const useJobStore = create<JobState & JobAction>()((set) => ({
-  jobList: [],
+  // Initial state for the job store
+  jobList: [], // Start with an empty job list
 
+  // Action to update the job list
   updateJobList: (jobList: Job[]) => {
+    // 'set' is used to update the state with the new jobList
     set(() => ({ jobList: jobList }));
   },
 }));
